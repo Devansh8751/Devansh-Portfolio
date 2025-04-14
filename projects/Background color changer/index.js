9636834f5d5c674c2c28a39146379a99ec6261fe
Devansh8751 <devansh8751.srg@gmail.com>
2025-04-14 08:37:05 +0530

    Delete projects directory

--- projects/Background color changer/index.js
+++
@@ -1,8 +0,0 @@
-function changeColor() {
-  const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
-  document.body.style.backgroundColor = randomColor;
-}
-
-let b = document.querySelector(".colorBtn");
-
-b.addEventListener("click", changeColor);
